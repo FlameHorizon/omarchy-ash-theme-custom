0e2258b1b25e9624c481ad49ff39ef751d419e52
Maciej Piotrowski <maciejpiotrowsk@gmail.com>
2025-11-10 19:59:11 +0100

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,12 +1,23 @@
 return {
 	{
-		"bjarneo/ash.nvim",
-		name = "ash",
+		"ficcdaf/ashen.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = {
+				bold = false,
+				italic = false,
+			},
+			colors = {
+				g_1 = "#d3c6aa",
+				g_2 = "#d3c6aa",
+			},
+		},
 	},
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "ash",
+			colorscheme = "ashen",
 		},
 	},
 }
